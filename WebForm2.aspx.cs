ff320bb6d65f093768ac1a3c826746e0715cfc54
thomasfregene <fregzythomas@gmail.com>
2020-10-27 11:47:43 +0100

    Reading the connectionString from web.config file

--- WebForm2.aspx.cs
+++ WebForm2.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -13,10 +14,11 @@ namespace Ado.NetIntro
         protected void Page_Load(object sender, EventArgs e)
         {
             //SqlConnection string
-            string CS = "data source=DESKTOP-44J960K\\SQLEXPRESS; database= Sample1; integrated security=SSPI";
+            //string CS = "data source=DESKTOP-44J960K\\SQLEXPRESS; database= Sample1; integrated security=SSPI";
 
 
             //to ensure connection is closed as soon as possible use the trycatch and finally or use the using block
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
             using (SqlConnection con = new SqlConnection(CS))
             {
                 SqlCommand cmd = new SqlCommand("SELECT * FROM tblProduct", con);

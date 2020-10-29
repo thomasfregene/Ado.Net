6073239a02a6286866d8fa1feb3c8dfa0c8e78ae
thomasfregene <fregzythomas@gmail.com>
2020-10-29 11:34:45 +0100

    looping through data using dataReader

--- WebForm6.aspx.cs
+++ WebForm6.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -15,14 +16,37 @@ namespace Ado.NetIntro
         {
             string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
 
-            using(SqlConnection con = new SqlConnection(CS))
+            using (SqlConnection con = new SqlConnection(CS))
             {
                 con.Open();
                 SqlCommand cmd = new SqlCommand("SELECT ProductId, ProductName, UnitPrice FROM tblProductInventory2", con);
-                //dataReader object
-                SqlDataReader rdr = cmd.ExecuteReader();
-                GridView1.DataSource = rdr;
-                GridView1.DataBind();
+                //dataReader object the using is neccesary to close the reader
+                using (SqlDataReader rdr = cmd.ExecuteReader())
+                {
+                    //doing additional manipulation of initial data
+                    DataTable table = new DataTable();
+                    table.Columns.Add("ID");
+                    table.Columns.Add("Name");
+                    table.Columns.Add("Price");
+                    table.Columns.Add("DiscountedPrice");
+
+                    while (rdr.Read())
+                    {
+                        DataRow dataRow = table.NewRow();
+
+                        int originalPrice = Convert.ToInt32(rdr["UnitPrice"]);
+                        double discountedPrice = originalPrice * 0.9;
+
+                        //assocating the original columns to a dataRow
+                        dataRow["ID"] = rdr["ProductId"];
+                        dataRow["Name"] = rdr["ProductName"];
+                        dataRow["Price"] = originalPrice;
+                        dataRow["DiscountedPrice"] = discountedPrice;
+                        table.Rows.Add(dataRow);
+                    }
+                    GridView1.DataSource = table;
+                    GridView1.DataBind();
+                }
             }
         }
     }

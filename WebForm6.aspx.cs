1b4fc9f3692e8bf1d0fc692daf3521181d48ec91
thomasfregene <fregzythomas@gmail.com>
2020-10-29 11:12:51 +0100

    using dataReader to read data from database

---
+++ WebForm6.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ado.NetIntro
+{
+    public partial class WebForm6 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+            using(SqlConnection con = new SqlConnection(CS))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT ProductId, ProductName, UnitPrice FROM tblProductInventory2", con);
+                //dataReader object
+                SqlDataReader rdr = cmd.ExecuteReader();
+                GridView1.DataSource = rdr;
+                GridView1.DataBind();
+            }
+        }
+    }
+}
